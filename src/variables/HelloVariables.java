806db288107425d797482a8959496694dcbbaebe
Fabiane <fabiane.ypiranga@dedalus.com>
2026-03-30 14:40:50 -0300

    feat(variables): declare and print int, double, and String

---
+++ src/variables/HelloVariables.java
@@ -0,0 +1,11 @@
+public class HelloVariables {
+    public static void main(String[] args) {
+        int idade = 20;
+        double altura = 1.70;
+        String nome = "Fabiane";
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+    }
+}

fe265d1ae0b8d3431c92f1f82b4b380729b63374
Fabiane <fabiane.ypiranga@dedalus.com>
2026-05-05 09:58:17 -0300

    feat(control-flow): add for loop example printing numbers 1 to 5

---
+++ src/controlflow/ForLoopExample.java
@@ -0,0 +1,12 @@
+package controlflow;
+
+public class ForLoopExample {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Número: " + i);
+        }
+
+    }
+}

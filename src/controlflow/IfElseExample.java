e5681734f9f01968b67ed5ff254eecff570030c3
Fabiane <fabiane.ypiranga@dedalus.com>
2026-05-05 09:52:41 -0300

    feat(control-flow): add if/else example for age check

---
+++ src/controlflow/IfElseExample.java
@@ -0,0 +1,14 @@
+package controlflow;
+
+public class IfElseExample {
+
+    public static void main(String[] args) {
+        int age = 18;
+
+        if (age >= 18) {
+            System.out.println("Maior de idade");
+        } else {
+            System.out.println("Menor de idade");
+        }
+    }
+}
